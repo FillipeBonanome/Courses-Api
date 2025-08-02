c31cfd81524c9f4d0a7ffee56e6d85513cc13d0b
Fillipe <incarnialol2@gmail.com>
2025-08-02 13:32:02 -0300

    feat: add course entity
    - Added Course Entity (missing lessons for now)

---
+++ src/main/java/com/courses/api/Api/entity/Course.java
@@ -0,0 +1,40 @@
+package com.courses.api.Api.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String description;
+    @NotBlank
+    private String category;
+    private String thumbnailFileName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "instructor_id", referencedColumnName = "id")
+    private User instructor;
+    private Boolean published;
+    private String slug;
+    //TODO --> add lessons
+    //List<Lesson> lessons = new ArrayList<>()
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+}
